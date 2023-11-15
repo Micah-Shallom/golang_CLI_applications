d835f8cdf915ba943a6049d2649a4c33a3306afb
Micah <micahshallom@gmail.com>
2023-11-15 17:14:45 +0100

    implemented test for counting bytes functionality

--- wordcounter/main_test.go
+++ wordcounter/main_test.go
@@ -8,7 +8,7 @@ import (
 func TestCountWord(t *testing.T) {
 	b := bytes.NewBufferString("word1 word2 word3 word4\n")
 	exp := 4
-	res := count(b, false)
+	res := count(b, false, false)
 	if res != exp {
 		t.Errorf("Expected %d, got %d instead. \n", exp, res)
 	}
@@ -17,9 +17,18 @@ func TestCountWord(t *testing.T) {
 func TestCountLines(t *testing.T){
 	b :=bytes.NewBufferString("word1 word2 word3 \n line2 \n line3 word1")
 	exp := 3
-	res := count(b, true)
+	res := count(b, true, false)
 
 	if res != exp {
 		t.Errorf("Expected %d, got %d instead. \n", exp, res)
+	}
+}
+
+func TestCountBytes(t *testing.T){
+	b := bytes.NewBufferString("word1 word2 word3")
+	exp := 17
+	res := count(b, false, true)
+	if res != exp {
+		t.Errorf("Expected %d, got %d instead.\n", exp, res)
 	}
 }
